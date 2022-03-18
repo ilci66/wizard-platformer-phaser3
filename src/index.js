848e8de1be39fb3b8030a55e07929e7c94a7ec61
unknown <akbiyik.ilker7@gmail.com>
2022-03-18 22:01:03 +0100

    working on bg

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import wizardImg from './assets/wizard.png';
 import wizardJson from './assets/wizard.json';
+import bgImg from './assets/background_layer_3.png';
 
 class MyGame extends Phaser.Scene
 {
@@ -14,20 +15,32 @@ class MyGame extends Phaser.Scene
     {
         this.load.image('logo', logoImg);
         this.load.atlas('wizard', wizardImg, wizardJson);
+        this.load.image('bg', bgImg);
+        
     }
-      
+    
     create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
+    {   
+        // let background = this.add.image(400, 16, 'bg').setOrigin(0, 0);
+        // I was using the code above for my game but
+        // found this code online that strethes my background
+        let background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg')
+        let scaleX = this.cameras.main.width / image.width
+        let scaleY = this.cameras.main.height / image.height
+        let scale = Math.max(scaleX, scaleY)
+        image.setScale(scale).setScrollFactor(0)
+
+
+        // const logo = this.add.image(400, 150, 'logo');
       
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 450,
+        //     duration: 2000,
+        //     ease: "Power2",
+        //     yoyo: true,
+        //     loop: -1
+        // });
         this.anims.create({
             key: 'idle',
             frames: this.anims.generateFrameNames('wizard', { prefix: 'idle00', start: 0, end: 5, suffix: '.png'}),
@@ -36,6 +49,10 @@ class MyGame extends Phaser.Scene
         });
         var wizard = this.add.sprite(100, 100);
         wizard.play('idle')
+    }
+    update ()
+    {
+        background.tilePosition.x += 0.5; 
     }
 }
 
